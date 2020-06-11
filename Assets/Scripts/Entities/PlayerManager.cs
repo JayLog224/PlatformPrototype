584775f25f1c9f7b813841c07ee5ab086e7a5354
JayLog22 <jaylog224@gmail.com>
2020-06-11 01:38:29 -0400

    Initial push with dying animation and spikes that kill you

---
+++ Assets/Scripts/Entities/PlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Transform respawnPoint;
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Deadly")
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    void SetRespawnPosition()
+    {
+        transform.position = respawnPoint.transform.position;
+    }
+
+    IEnumerator Die()
+    {
+        animator.SetBool("HasDied", true);
+        yield return new WaitForSeconds(0.4f);
+        Respawn();
+    }
+
+    void Respawn()
+    {
+        animator.SetBool("HasDied", false);
+        transform.position = respawnPoint.transform.position;
+    }
+}
