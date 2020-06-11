963d3911f1cc128205dfb77ab85abeb49b894b77
JayLog22 <jaylog224@gmail.com>
2020-06-10 21:26:23 -0400

    Initial Commit

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameUtils;
+
+namespace Managers
+{
+    public class InputManager : SingletonObject<InputManager>
+    {
+        public float AxisHorizontal { get { return Input.GetAxisRaw("Horizontal"); } }
+        public bool AxisVertical { get { return Input.GetButtonDown("Jump"); } }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 10f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    [SerializeField]
+    Animator animator;
+
+    void Update()
+    {
+        horizontalMove = InputManager.Instance.AxisHorizontal * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (InputManager.Instance.AxisVertical)
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+}
